23e8c6ca984dc958e453c328b1707f13c0eb6ee4
Chase Givelekian <mc_cgivelekian25@mcjvs2.org>
2025-04-22 13:51:17 -0400

    Created the world object. Need to mess with the atmosphere values.

--- Assets/Scripts/Generation/Terrain/LodMeshLoader.cs
+++ Assets/Scripts/Generation/Terrain/LodMeshLoader.cs
@@ -1,69 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
-using Generation.Terrain;
 using UnityEngine;
 using Seb.Meshing;
 
 public class LodMeshLoader : MonoBehaviour
 {
-    public TextAsset meshFileHighRes;
-    public TextAsset meshFileLowRes;
+	public TextAsset meshFileHighRes;
+	public TextAsset meshFileLowRes;
 
-    public Material mat;
-    public Material lowResMat;
-    public bool useStaticBatching;
-    public bool loadOnStart;
-    public SimpleLodSystem lodSystem;
-
-
-    void Start()
-    {
-        if (loadOnStart)
-        {
-            Load();
-        }
-    }
-
-    public void Load()
-    {
-        MeshRenderer[] highResRenderers = CreateRenderers(meshFileHighRes, mat);
-        MeshRenderer[] lowResRenderers = CreateRenderers(meshFileLowRes, lowResMat);
-
-        Debug.Assert(highResRenderers.Length == lowResRenderers.Length,
-            "Mismatch in number of high and low res meshes");
-
-        for (int i = 0; i < highResRenderers.Length; i++)
-        {
-            lodSystem.AddLOD(highResRenderers[i], lowResRenderers[i]);
-        }
-    }
-
-    MeshRenderer[] CreateRenderers(TextAsset loadFile, Material material)
-    {
-        SimpleMeshData[] meshData = MeshSerializer.BytesToMeshes(loadFile.bytes);
-        MeshRenderer[] meshRenderers = new MeshRenderer[meshData.Length];
-        GameObject[] allObjects = new GameObject[meshData.Length];
+	public Material mat;
+	public Material lowResMat;
+	public bool useStaticBatching;
+	public bool loadOnStart;
+	public SimpleLodSystem lodSystem;
 
 
-        for (int i = 0; i < meshRenderers.Length; i++)
-        {
-            var renderObject = MeshHelper.CreateRendererObject(meshData[i].name, meshData[i], material,
-                parent: transform, gameObject.layer);
+	void Start()
+	{
+		if (loadOnStart)
+		{
+			Load();
+		}
+	}
 
-            meshRenderers[i] = renderObject.renderer;
-            allObjects[i] = renderObject.gameObject;
+	public void Load()
+	{
+		MeshRenderer[] highResRenderers = CreateRenderers(meshFileHighRes, mat);
+		MeshRenderer[] lowResRenderers = CreateRenderers(meshFileLowRes, lowResMat);
 
-            if (useStaticBatching)
-            {
-                meshRenderers[i].gameObject.isStatic = true;
-            }
-        }
+		Debug.Assert(highResRenderers.Length == lowResRenderers.Length, "Mismatch in number of high and low res meshes");
 
-        if (useStaticBatching)
-        {
-            StaticBatchingUtility.Combine(allObjects, gameObject);
-        }
+		for (int i = 0; i < highResRenderers.Length; i++)
+		{
+			lodSystem.AddLOD(highResRenderers[i], lowResRenderers[i]);
+		}
 
-        return meshRenderers;
-    }
+
+	}
+
+	MeshRenderer[] CreateRenderers(TextAsset loadFile, Material material)
+	{
+		SimpleMeshData[] meshData = MeshSerializer.BytesToMeshes(loadFile.bytes);
+		MeshRenderer[] meshRenderers = new MeshRenderer[meshData.Length];
+		GameObject[] allObjects = new GameObject[meshData.Length];
+
+
+		for (int i = 0; i < meshRenderers.Length; i++)
+		{
+			var renderObject = MeshHelper.CreateRendererObject(meshData[i].name, meshData[i], material, parent: transform, gameObject.layer);
+
+			meshRenderers[i] = renderObject.renderer;
+			allObjects[i] = renderObject.gameObject;
+
+			if (useStaticBatching)
+			{
+				meshRenderers[i].gameObject.isStatic = true;
+			}
+		}
+
+		if (useStaticBatching)
+		{
+			StaticBatchingUtility.Combine(allObjects, gameObject);
+		}
+
+		return meshRenderers;
+	}
+
 }
